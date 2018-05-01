0a1851361bc583b80b879fa0f49421e0b90118a1
Tomagi <tomergueta@gmail.com>
2018-05-01 10:18:06 +0300

    Tomer Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  myArray: Array<number> = [1,2,3];
+
+  getMyName(): string {
+    return 'Tomer Gueta';
+  }
+
+  addElements(): void {
+    this.myArray.push(Math.random() * 20);
+  }
+
+  subElements(): void {
+    //this.myArray.shift();
+    this.myArray.splice(0,5);
+  }
 }
